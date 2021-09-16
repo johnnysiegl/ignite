b43349bb46672a49ea0fae66841a27cbc4852891
johnny.junior <johnny.junior@pentacom.solutions>
2021-09-15 21:21:48 -0300

    Finish chapther 2 - dtMoney complete

--- reactjs/02-dtmoney/src/App.tsx
+++ reactjs/02-dtmoney/src/App.tsx
@@ -1,14 +1,32 @@
 import { Dashoard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react'
+import { NewTransactionModal } from "./Components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
   return (
-    <div>
-      <Header />
+    <TransactionsProvider>
+      <Header onOpenNewTransacionModal={handleOpenNewTransactionModal} />
       <Dashoard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
-    </div>
+    </ TransactionsProvider>
   );
 }
 
